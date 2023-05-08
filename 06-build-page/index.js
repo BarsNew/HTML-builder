8a34667c2d958d966073a8e673c915f50f2d15df
BarsNew <Bars11021988@mail.ru>
2023-05-08 21:19:20 +0300

    fix: correction 6 task (Пн, 08 Май 2023,21:19:20)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -12,28 +12,44 @@ const newF = './06-build-page/project-dist';
 const oldStyles = 'styles';
 const newStyles = 'style.css';
 
+const templatePath = './06-build-page/template.html';
+const componentsPath = './06-build-page/components';
+
+const newIndex = './06-build-page/project-dist/index.html';
+
 copyDir(oldAssets, newAssets) 
   .then(() => console.log('Копирование assets завершено'))
   .catch(err => console.error('Ошибка копирования:', err));
 
 buildStyles(oldStyles, newStyles, oldF, newF)
-  .then(() => console.log('Объединение стилей завершено!'))
+  .then(() => console.log('Объединение стилей завершено'))
   .catch((err) => console.error('Ошибка объединения стилей:', err));
 
-const templatePath = './06-build-page/template.html';
-const headerPath = './06-build-page/components/header.html';
-const articlesPath = './06-build-page/components/articles.html';
-const footerPath = './06-build-page/components/footer.html';
+async function buildPage(template, component, directory, inFile) {
+  const [newTemplate, files] = await Promise.all([
+    fs.readFile(template, 'utf8'),
+    fs.readdir(component, { withFileTypes: true })
+  ]);
 
-Promise.all([
-  fs.readFile(templatePath, 'utf8'),
-  fs.readFile(headerPath, 'utf8'),
-  fs.readFile(articlesPath, 'utf8'),
-  fs.readFile(footerPath, 'utf8')
-]).then(([template, header, articles, footer]) => {
-  const html = template.replace('{{header}}', header).replace('{{articles}}', articles).replace('{{footer}}', footer);
+  const components = {};
+  for (const file of files) {
+    if (file.isFile() && path.extname(file.name) === '.html') {
+      const name = path.basename(file.name, '.html');
+      components[name] = await fs.readFile(path.join(component, file.name), 'utf8');
+    }
+  }
 
-  fs.writeFile('./06-build-page/project-dist/index.html', html);
-}).catch((err) => {
-  console.error(err);
-});  
+  let html = newTemplate;
+  for (const name in components) {
+    const tag = `{{${name}}}`;
+    const content = components[name];
+    html = html.replace(tag, content);
+  }
+
+  await fs.mkdir(directory, { recursive: true });
+  await fs.writeFile(inFile, html);
+}
+
+buildPage(templatePath, componentsPath, newF, newIndex )
+  .then(() => console.log('Замен шаблонных тегов завершена'))
+  .catch((err) => console.error(err));
